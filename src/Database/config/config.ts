bc3035372bb2359188480f9a08c43a3f64fe655e
Ray Ch <i@iraycd.com>
2019-09-30 17:48:01 +0530

    eslint fixes

--- src/Database/config/config.ts
+++ src/Database/config/config.ts
@@ -1,5 +1,5 @@
 const configJSON = {
-  ARANGO_URL:"http://db:8529",
+  ARANGO_URL: "http://db:8529",
   ARANGO_DB: "randomtest"
 };
 export default configJSON;
--- src/Query/Game/listGameQuery.test.ts
+++ src/Query/Game/listGameQuery.test.ts
@@ -3,13 +3,13 @@ import ArangoDB from "../../Database/models";
 import configJSON from "../../Database/config/config";
 
 describe("Get Game Query", () => {
-  beforeAll(async()=>{
-    const systemDB = ArangoDB.db.useDatabase('_system');
+  beforeAll(async () => {
+    const systemDB = ArangoDB.db.useDatabase("_system");
     await systemDB.dropDatabase(configJSON.ARANGO_DB);
     await systemDB.createDatabase(configJSON.ARANGO_DB);
     const testDB = ArangoDB.db.useDatabase(configJSON.ARANGO_DB);
-    await testDB.collection('game').create()
-  })
+    await testDB.collection("game").create();
+  });
   it("Get Game Query", async done => {
     const api = await apiHelper();
     const todo = await api.query({
